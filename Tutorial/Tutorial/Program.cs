fff3ef7a6f97c3ed1a9e6018bc08cfcef7e56260
gcherry <gcherry2@gmail.com>
2021-10-09 20:14:05 -0400

    Initial run with bot connected to discord server

---
+++ Tutorial/Tutorial/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace Tutorial
+{
+    static class Program
+    {
+        public static async Task Main(string[] args)
+            => await Startup.RunAsync(args);
+    }
+}
